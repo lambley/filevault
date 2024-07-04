a54e43580d9d8159836e10c7fa77e1bf49fa84a8
Aaron Lambley <aaron.lambley@gmail.com>
2024-07-04 10:38:26 +0100

    refactor: separate app and index for easier testing

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import dotenv from "dotenv";
+import uploadRoutes from "./routes/uploadRoutes";
+import filesRoutes from "./routes/filesRoutes";
+import deleteRoutes from "./routes/deleteRoutes";
+
+dotenv.config();
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000"];
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.use("/upload", uploadRoutes);
+app.use("/files", filesRoutes);
+app.use("/files", deleteRoutes);
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,6 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
-import dotenv from "dotenv";
-import uploadRoutes from "./routes/uploadRoutes";
-import filesRoutes from "./routes/filesRoutes";
-import deleteRoutes from "./routes/deleteRoutes";
+import app from './app';
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const allowedOrigins = ["http://localhost:3000"];
-const corsOptions: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
-
-app.use(cors(corsOptions));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-
-app.use("/upload", uploadRoutes);
-app.use("/files", filesRoutes);
-app.use("/files", deleteRoutes);
+const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
